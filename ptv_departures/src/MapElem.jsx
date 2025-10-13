3b9796b2dcc5b02cefabe68466b2ca5507a42774
User Name <evergreen9898@outlook.com>
2025-10-13 20:50:07 +1100

    updated favicon

--- ptv_departures/src/MapElem.jsx
+++ ptv_departures/src/MapElem.jsx
@@ -11,7 +11,7 @@ export default function MapElem({pos, selectedStop, stopsList}) {
     }, [pos, map]);
     return (
         <div className="width50 height100">
-            <MapContainer ref={setMap} center={[-37.8136,144.9631]} zoom={15} scrollWheelZoom={true} style={{width: "100%", height: "100%"}}>
+            <MapContainer center={[-37.8136,144.9631]} zoom={15} scrollWheelZoom={true} ref={setMap} style={{width: "100%", height: "100%"}}>
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
