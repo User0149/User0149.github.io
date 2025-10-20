908a024714c7433231a547283f3df6c4c42fcb8e
User Name <evergreen9898@outlook.com>
2025-10-20 23:23:54 +1100

    don't load map if empty

--- ptv_departures/src/MapElem.jsx
+++ ptv_departures/src/MapElem.jsx
@@ -13,14 +13,7 @@ export default function MapElem({realPos, pos, setPos, selectedStop, setSelected
     
     if (!stopsList && !selectedStop) {
         return (
-            <div className="height100" style={{width: "40%"}}>
-                <MapContainer center={[-37.8136, 144.9631]} zoom={15} scrollWheelZoom={true} ref={setMap} style={{width: "100%", height: "100%"}}>
-                    <TileLayer
-                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                    />
-                </MapContainer>
-            </div>
+            <div className="height100" style={{width: "40%"}}></div>
         );
     }
 
