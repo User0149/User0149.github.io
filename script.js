2d17e3bc2cc06c1efc252f6a6e8ad5e62b6ec96c
User0149 <floatingprecision@gmail.com>
2025-11-11 16:49:35 +1100

    updated error handling

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 (async () => {
-    const response = await fetch("https://api.github.com/repos/User0149/User0149.github.io/contents/");
-    if (!response.ok) alert("Could not fetch data.");
+    try {
+        const response = await fetch("https://api.github.com/repos/User0149/User0149.github.io/contents/");
+        const sites = await response.json();
+        
+        for (const site of sites) {
+            if (site.type !== "dir") continue;
+            let div = document.createElement('div');
 
-    const sites = await response.json();
-    
-    for (const site of sites) {
-        if (site.type !== "dir") continue;
-        let div = document.createElement('div');
+            let link = document.createElement('a');
+            link.href = `https://user0149.github.io/${site.path}`;
+            link.innerText = `https://user0149.github.io/${site.path}`;
 
-        let link = document.createElement('a');
-        link.href = `https://user0149.github.io/${site.path}`;
-        link.innerText = `https://user0149.github.io/${site.path}`;
-
-        div.appendChild(link);
-        document.body.appendChild(div);
+            div.appendChild(link);
+            document.body.appendChild(div);
+        }
+    }
+    catch (err) {
+        alert("An error has occurred. Check console for details.");
+        console.error(err);
     }
 })();
