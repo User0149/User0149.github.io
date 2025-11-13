d778adfc87aea82d68f6fa8d853c3b55e9c8e8c5
User0149 <floatingprecision@gmail.com>
2025-11-13 16:15:56 +1100

    restructure files

--- dashboard/tables.js
+++
@@ -1,7 +0,0 @@
-var tables=0;
-document.addEventListener("DOMContentLoaded",()=>{
-    var tables_element=document.getElementById("tables").style
-    tables_element.textAlign="center";
-    tables_element.margin="auto";
-    tables_element.width=`${tables*191}px`;
-});
