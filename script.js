2b23b48fc563e261df8c3f9d12dd34f7fb2525f3
User0149 <floatingprecision@gmail.com>
2025-11-13 16:44:40 +1100

    made the home page look slightly less bad

--- script.js
+++ script.js
@@ -1,22 +1,13 @@
-(async () => {
-    try {
-        const response = await fetch("https://api.github.com/repos/User0149/User0149.github.io/contents/");
-        const sites = await response.json();
-        
-        for (const site of sites) {
-            if (site.type !== "dir") continue;
-            let div = document.createElement('div');
+const sites = ["ptv_departures", "dashboard", "notepad", "colour_picker", "refraction_simulator"];
+const sitesDiv = document.getElementById("sites_div");
 
-            let link = document.createElement('a');
-            link.href = `https://user0149.github.io/${site.path}`;
-            link.innerText = `https://user0149.github.io/${site.path}`;
+for (const site of sites) {
+    let div = sitesDiv.createElement('div');
 
-            div.appendChild(link);
-            document.body.appendChild(div);
-        }
-    }
-    catch (err) {
-        alert("An error has occurred. Check console for details.");
-        console.error(err);
-    }
-})();
+    let link = sitesDiv.createElement('a');
+    link.href = `https://user0149.github.io/${site.path}`;
+    link.innerText = `https://user0149.github.io/${site.path}`;
+
+    div.appendChild(link);
+    sitesDiv.appendChild(div);
+}
