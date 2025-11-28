c4528efc9fef1d7f9b3c73ba644be5d4729d69b8
User0149 <floatingprecision@gmail.com>
2025-11-28 16:39:18 +1100

    added public folder

--- src/ProjectCard.tsx
+++ src/ProjectCard.tsx
@@ -3,7 +3,7 @@ import type { projectType } from "./types";
 export default function ProjectCard({project}: {project: projectType}) {
     return (
         <div className="project-card space-y-3">
-            <img src={"./src/assets/"+project.image} className="w-full"/>
+            <img src={"./assets/"+project.image} className="w-full"/>
             <div className="content-container-small space-y-3">
                 <h3><a className="text-xl underline" href={project.href}>{project.name}</a></h3>
                 <p>{project.description}</p>
