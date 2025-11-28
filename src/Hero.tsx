adb864f2a49454997cd19e03026aae1fb3b1f98e
User0149 <floatingprecision@gmail.com>
2025-11-28 18:51:34 +1100

    added jumping and sliding animations

--- src/Hero.tsx
+++ src/Hero.tsx
@@ -1,14 +1,14 @@
 export default function Hero() {
     return (
-        <section className="h-screen relative bg-[url('./assets/profile_photo.svg')] bg-no-repeat">
+        <section className="h-screen relative bg-[url('./assets/profile_photo.svg')] bg-no-repeat bg-slide-in">
             <div className="h-full w-full absolute top-0 left-0 bg-[gray] opacity-10 z-0"></div>
             <div className="content-container">
                 <div className="relative z-1 pt-40 space-y-4">
                     <div>
-                        <h1 className="text-center text-7xl">Welcome to my</h1>
-                        <h1 className="text-center text-7xl">GitHub!</h1>
+                        <h1 className="text-center text-7xl jump-up-down">Welcome to my</h1>
+                        <h1 className="text-center text-7xl jump-up-down" style={{animationDelay: "300ms"}}>GitHub!</h1>
                     </div>
-                    <div className="mx-auto text-center text-3xl border w-60 p-3 rounded-full cursor-pointer"><a href="#projects">View projects</a></div>
+                    <div className="mx-auto text-center text-3xl border w-60 p-3 rounded-full cursor-pointer jump-up-down" style={{animationDelay: "500ms"}}><a href="#projects">View projects</a></div>
                 </div>
             </div>
         </section>
