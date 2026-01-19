dae0dc0edf8a37c0655a62a8e54ebf8b9235e64d
User0149 <floatingprecision@gmail.com>
2026-01-19 18:14:54 +1100

    reorder imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+
+import "./styles/animations.css";
+
+import SpaceY from "./components/SpaceY";
+
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
 import Footer from "./components/Footer";
-import "./styles/animations.css";
-import SpaceY from "./components/SpaceY";
 
 function App() {
     return (
